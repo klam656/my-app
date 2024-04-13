45255732180999c718ed1b830501ef9822e94a1d
Kenny Lam <lamkenny1010@gmail.com>
2024-04-13 16:38:29 +1200

    Feat: Enable user to read blog post details.

--- app/blogs/[blogId]/page.js
+++ app/blogs/[blogId]/page.js
@@ -1,3 +1,27 @@
-export default function BlogDetailPage() {
-  return <div>BlogDetailPage</div>;
+async function getData(postId) {
+  const url = "http://localhost:3000/blogs.json";
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const specificPost = data.find((blog) => blog.id == postId);
+  return specificPost;
+}
+
+export default async function BlogDetailPage({ params }) {
+  try {
+    const data = await getData(Number(params.blogId));
+    return (
+      <main>
+        <h1>
+          Blog {data.id}: {data.tittle}
+        </h1>
+        <ul>{data.content}</ul>
+      </main>
+    );
+  } catch (error) {
+    return <main>Error: {error.message}</main>; // Display error message if fetching fails
+  }
 }
