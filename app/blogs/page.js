2f2627dbc356fd89eada5d8a4803fe34c93b7638
Kenny Lam <lamkenny1010@gmail.com>
2024-04-13 16:38:11 +1200

    Feat: Displayed list of blog posts.

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,3 +1,34 @@
-export default function Blogs() {
-  return <div>Blogs</div>;
+import Link from "next/link";
+
+async function getData() {
+  const url = "http://localhost:3000/blogs.json";
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Blogs() {
+  try {
+    const data = await getData();
+    return (
+      <main>
+        <h1>Blog Titles</h1>
+        <ul>
+          {data.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blogs/${blog.id}`}>
+                Blog {blog.id}: {blog.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  } catch (error) {
+    return <main>Error: {error.message}</main>; // Display error message if fetching fails
+  }
 }
